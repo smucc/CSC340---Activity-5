bbd83b1814b13c2d50859a35831614ad7f8b2a60
sebtardieu02 <sebastiantardieu02@gmail.com>
2024-04-14 18:26:30 -0400

    IMPROVED LAYOUT

--- three.py
+++ three.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 import mysql.connector
 import decimal
-
-import mysql.connector
-import tkinter as tk
+from PIL import Image, ImageTk
+from tkinter import PhotoImage
 
 class IceCreamShopApp:
     def __init__(self, master):
@@ -14,17 +13,30 @@ class IceCreamShopApp:
         self.mydb = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="20Horses!",
+            password="fantasyape123",
             database="IceCream"
         )
+
+        # Creating frames for each category
+        self.base_frame = tk.Frame(master)
+        self.flavor_frame = tk.Frame(master)
+        self.toppings_frame = tk.Frame(master)
+        self.base_frame.grid(row=0, column=0)
+        self.flavor_frame.grid(row=1, column=0)
+        self.toppings_frame.grid(row=2, column=0)
+
+        # Initializing a list to hold PhotoImage objects
+        self.base_images = []
+        self.flavor_images = []
+        self.topping_images = []
 
         # Create frames for options and dividers
         self.create_frames_and_dividers()
 
         # Create listbox for selected items
         # Create listbox for selected items with larger font size
-        self.selected_items_listbox = tk.Listbox(master, width=50, height=18, font=("Arial", 14))
-        self.selected_items_listbox.grid(row=4, column=2, columnspan=2, padx=10, pady=10, sticky="nsew")
+        self.selected_items_listbox = tk.Listbox(master, width=50, height=20, font=("Arial", 14))
+        self.selected_items_listbox.grid(row=4, column=1, columnspan=3, padx=10, pady=10, sticky="nsew")
 
         # Create confirm buttons for base, flavor, and topping selection
         self.create_confirm_buttons()
@@ -34,14 +46,14 @@ class IceCreamShopApp:
         self.show_flavors_options()
         self.show_toppings_options()
 
-        # Create reset order button
-        self.create_reset_button()
-        self.create_complete_order_button()
-
         # Create rating buttons
         self.create_bad_rating_button()
         self.create_ok_rating_button()
         self.create_great_rating_button()
+
+        # Create reset order button
+        self.create_reset_button()
+        self.create_complete_order_button()
 
         # State variables to track confirmation status
         self.base_confirmed = False
@@ -53,9 +65,6 @@ class IceCreamShopApp:
         self.selected_base = None
         self.selected_flavor = None
         self.selected_toppings = []
-
-        self.total_price = decimal.Decimal('0.0')
-        #self.total_price = 0.0  # Initialize total price variable
 
         # Labels for displaying most popular items
         self.most_popular_base_label = tk.Label(master, text="", font=("Arial", 16))
@@ -73,8 +82,16 @@ class IceCreamShopApp:
         # Display the most popular items
         self.display_most_popular_items()
 
+        self.total_price = decimal.Decimal('0.0')
+        #self.total_price = 0.0  # Initialize total price variable
 
     def create_frames_and_dividers(self):
+        # Configuring grid rows and columns for flexibility
+        self.master.grid_rowconfigure(0, weight=1)
+        self.master.grid_columnconfigure(0, weight=1, minsize=300)
+        self.master.grid_columnconfigure(2, weight=1, minsize=300)
+        self.master.grid_columnconfigure(4, weight=1, minsize=300)
+
         # Create frame for base options
         self.base_options_frame = tk.Frame(self.master)
         self.base_options_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
@@ -89,17 +106,17 @@ class IceCreamShopApp:
 
         # Create divider frames
         divider1 = tk.Frame(self.master, bg="black", width=2)
-        divider1.grid(row=0, column=1, rowspan=4, padx=10, pady=10, sticky="ns")
+        divider1.grid(row=1, column=1, rowspan=3, padx=10, pady=10, sticky="ns")
         divider2 = tk.Frame(self.master, bg="black", width=2)
-        divider2.grid(row=0, column=3, rowspan=4, padx=10, pady=10, sticky="ns")
+        divider2.grid(row=1, column=3, rowspan=3, padx=10, pady=10, sticky="ns")
 
         divider3 = tk.Frame(self.master, bg="black", width=50)
-        divider3.grid(row=3, column=0, columnspan=5, padx=10, pady=10, sticky="ew")
+        divider3.grid(row=3, column=10, columnspan=5, padx=10, pady=5, sticky="ew")
 
         # Create label to display total price
         # Create label to display total price
         self.total_label = tk.Label(self.master, text="TOTAL: $0.00", font=("Arial", 24))
-        self.total_label.grid(row=4, column=3, columnspan=3, padx=20, pady=40, sticky="s")
+        self.total_label.grid(row=4, column=4, columnspan=15, padx=50, pady=70, sticky="s")
 
         self.bases_label = tk.Label(self.master, text="Select a Base", font=("Arial", 24))
         self.bases_label.grid(row=0, column=0, sticky="n")
@@ -143,7 +160,11 @@ class IceCreamShopApp:
                                                font=("Arial", 10), command=self.give_great_rating)
         self.great_rating_button.grid(row=4, column=1, padx=10, pady=10, sticky="s")
 
+
     def add_to_cart(self, option, price, category):
+       # Debug: Output when adding to cart
+        print(f"Adding to cart: {option}, {price}, {category}")
+
         # Format the option, category, and price to display in the listbox
         formatted_item = f"{option} ({category})"
         if price is not None:
@@ -153,10 +174,9 @@ class IceCreamShopApp:
             padded_item = f"{formatted_item}{' ' * padding_length}{formatted_price}"
             item = padded_item
             # Update total price
-            self.total_price += price
+            self.total_price += decimal.Decimal(str(price))
         else:
             item = formatted_item
-
         # Check if the item is already in the list
         if item not in self.selected_items_listbox.get(0, tk.END):
             # Append the item to the list
@@ -169,52 +189,94 @@ class IceCreamShopApp:
 
         # Display the total price at the bottom of the list
         self.display_total_price()
-
     def display_total_price(self):
         # Check if the total price item is already in the list
+        total_indices = [i for i, item in enumerate(self.selected_items_listbox.get(0, tk.END)) if "Total" in item]
+        for index in reversed(total_indices):
+            self.selected_items_listbox.delete(index)
+
         total_price_item = f"Total: ${self.total_price:.2f}"
-        if total_price_item not in self.selected_items_listbox.get(0, tk.END):
+        self.selected_items_listbox.insert(tk.END, total_price_item)
+        # if total_price_item not in self.selected_items_listbox.get(0, tk.END):
             # Append the total price item to the list
-            self.selected_items_listbox.insert(tk.END, total_price_item)
+        total_price = f"Total: ${self.total_price:.2f}"
+        self.selected_items_listbox.insert(tk.END, total_price_item)
+
+        # Debug: Output current total price
+        print(f"Current total price: {self.total_price}")
 
     def show_base_options(self):
         cursor = self.mydb.cursor()
         cursor.execute("SELECT Base, Price FROM Base")
         options = cursor.fetchall()
-        num_options = len(options)
-        num_rows = (num_options + 2) // 3  # Number of rows needed for the grid
+        # num_options = len(options)
+        # num_rows = (num_options + 2) // 3  # Number of rows needed for the grid
+        image_filenames = ['images1.jpg', 'images2.jpg', 'images3.jpg', 'images4.jpg', 'images5.jpg']
+
         for i, (base, price) in enumerate(options):
+            img_path = image_filenames[i % len(image_filenames)]  # Cycle through images
+            img = Image.open(img_path)
+            img = img.resize((45, 45), Image.Resampling.LANCZOS)  # Resize image
+            photo = ImageTk.PhotoImage(img)
+
+            self.base_images.append(photo)  # Keep a reference to avoid garbage collection
+
             button_text = f"{base} - ${price:.2f}"
             command = lambda opt=base, prc=price: self.select_base(opt, prc)
             row_num = i // 3  # Determine the row number
-            col_num = i % 3   # Determine the column number
-            tk.Button(self.base_options_frame, text=button_text, command=command, width=17, height=4).grid(row=row_num, column=col_num, padx=5, pady=5)
+            col_num = i % 3  # Determine the column number
+            button = tk.Button(self.base_options_frame, text=button_text, image=photo, compound='top', command=command)
+            button.image = photo  # Keep reference to image
+            button.grid(row=row_num, column=col_num, padx=5, pady=5)
 
     def show_flavors_options(self):
         cursor = self.mydb.cursor()
         cursor.execute("SELECT Flavor, Price FROM Flavors")
         options = cursor.fetchall()
         num_options = len(options)
-        num_rows = (num_options + 2) // 3  # Number of rows needed for the grid
+        image_filenames = ['images6.jpg', 'images7.jpg', 'images8.jpg', 'images9.jpg', 'images10.jpg', 'images11.jpg', 'images12.jpg', 'images13.jpg', 'images14.jpg']
+
         for i, (flavor, price) in enumerate(options):
+            img_path = image_filenames[i % len(image_filenames)]  # Cycle through images
+            img = Image.open(img_path)
+            img = img.resize((45, 45), Image.Resampling.LANCZOS)  # Resize image
+            photo = ImageTk.PhotoImage(img)
+
+            self.flavor_images.append(photo)  # Keep a reference to avoid garbage collection
+
             button_text = f"{flavor} - ${price:.2f}"
             command = lambda opt=flavor, prc=price: self.select_flavor(opt, prc)
             row_num = i // 3  # Determine the row number
-            col_num = i % 3   # Determine the column number
-            tk.Button(self.flavor_options_frame, text=button_text, command=command, width=17, height=4, state=tk.DISABLED).grid(row=row_num, column=col_num, padx=5, pady=5)
+            col_num = i % 3  # Determine the column number
+            button = tk.Button(self.flavor_options_frame, text=button_text, image=photo, compound='top', command=command, state=tk.DISABLED)
+            button.image = photo  # Keep reference to image
+            button.grid(row=row_num, column=col_num, padx=5, pady=5)
+
+            #tk.Button(self.flavor_options_frame, text=button_text, command=command, width=17, height=4, state=tk.DISABLED).grid(row=row_num, column=col_num, padx=5, pady=5)
 
     def show_toppings_options(self):
         cursor = self.mydb.cursor()
         cursor.execute("SELECT Topping, Price FROM Toppings")
         options = cursor.fetchall()
-        num_options = len(options)
-        num_rows = (num_options + 2) // 3  # Number of rows needed for the grid
+        # num_options = len(options)
+        image_filenames = ['images15.jpg', 'images16.jpg', 'images17.jpg', 'images18.jpg', 'images19.jpg',
+                           'images20.jpg', 'images21.jpg', 'images22.jpg', 'images23.jpg']
+
         for i, (topping, price) in enumerate(options):
+            img_path = image_filenames[i % len(image_filenames)]  # Cycle through images
+            img = Image.open(img_path)
+            img = img.resize((45, 45), Image.Resampling.LANCZOS)  # Resize image
+            photo = ImageTk.PhotoImage(img)
+
+            self.topping_images.append(photo)  # Keep a reference to avoid garbage collection
+
             button_text = f"{topping} - ${price:.2f}"
             command = lambda opt=topping, prc=price: self.select_topping(opt, prc)
             row_num = i // 3  # Determine the row number
-            col_num = i % 3   # Determine the column number
-            tk.Button(self.topping_options_frame, text=button_text, command=command, width=17, height=4, state=tk.DISABLED).grid(row=row_num, column=col_num, padx=5, pady=5)
+            col_num = i % 3  # Determine the column number
+            button = tk.Button(self.topping_options_frame, text=button_text, image=photo, compound='top', command=command, state=tk.DISABLED)
+            button.image = photo  # Keep reference to image
+            button.grid(row=row_num, column=col_num, padx=5, pady=5)
 
 
     def select_base(self, base, price):
@@ -230,7 +292,6 @@ class IceCreamShopApp:
         self.total_price = price_decimal
         # Display the total price
         self.display_total_price()
-        self.final_base = base
 
         # Enable flavor buttons
         #for button in self.flavor_options_frame.winfo_children():
@@ -264,7 +325,6 @@ class IceCreamShopApp:
             #self.total_price = price_decimal
             # Display the total price
             self.display_total_price()
-            self.final_flavor = flavor
 
             # Enable confirm button
             self.flavor_confirm_button.config(state=tk.NORMAL)
@@ -376,39 +436,41 @@ class IceCreamShopApp:
         # Display the total price
         self.display_total_price()
 
+
     def complete_order(self):
-        # Get order information
-        order_base = self.final_base
-        order_flavor = self.final_flavor
-        order_toppings = ', '.join([topping[0] for topping in self.selected_toppings])
-        order_price = self.total_price
+        # Clear selected items list
+        self.selected_items_listbox.delete(0, tk.END)
 
-        # Insert order into database
-        cursor = self.mydb.cursor()
-        sql = "INSERT INTO Orders (base, flavor, toppings, price) VALUES (%s, %s, %s, %s)"
-        values = (order_base, order_flavor, order_toppings, order_price)
-        cursor.execute(sql, values)
-        self.mydb.commit()
+        # Reset confirmation status
+        self.base_confirmed = False
+        self.flavor_confirmed = False
+        self.topping_confirmed = False
 
-        # Reset order variables
-        self.reset_order()
+        # Reset selected items variables
+        self.selected_base = None
+        self.selected_flavor = None
+        self.selected_toppings = []
+
+        # Reset total price
+        self.total_price = decimal.Decimal('0.0')
+
+        # Reset buttons
+        self.base_confirm_button.config(state=tk.NORMAL)
+        self.flavor_confirm_button.config(state=tk.DISABLED)
+        self.topping_confirm_button.config(state=tk.DISABLED)
+
+        for button in self.base_options_frame.winfo_children():
+            button.config(state=tk.NORMAL)
+
+        for button in self.flavor_options_frame.winfo_children():
+            button.config(state=tk.DISABLED)
+
+        for button in self.topping_options_frame.winfo_children():
+            button.config(state=tk.DISABLED)
 
         # Display the total price
         self.display_total_price()
 
-        # Display most popular items
-        self.display_most_popular_items()
-
-    def display_most_recent_order(self):
-        # Fetch most recent order from database
-        cursor = self.mydb.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM Orders ORDER BY order_id DESC LIMIT 1")
-        recent_order = cursor.fetchone()
-
-        # Display the most recent order on the GUI
-        if recent_order:
-            order_info = f"Base: {recent_order['base']}, Flavor: {recent_order['flavor']}, Toppings: {recent_order['toppings']}, Price: ${recent_order['price']:.2f}"
-            self.most_recent_order_label.config(text=order_info)
 
     def display_most_popular_items(self):
         cursor = self.mydb.cursor()
@@ -454,7 +516,7 @@ class IceCreamShopApp:
         cursor.callproc("InsertRating", ("GREAT",))
         self.mydb.commit()
 
-
 root = tk.Tk()
 app = IceCreamShopApp(root)
 root.mainloop()
+
